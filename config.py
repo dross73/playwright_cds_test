6265667dfc8f7c8c2fa3de60cfdc7f8bb4626aa8
Dan Ross <evildead73@gmail.com>
2025-06-26 13:16:19 -0500

    Fix: use safe fallback values for credit card env vars

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # These are pulled from the .env file
-CREDIT_CARD_NUMBER = os.environ["CREDIT_CARD_NUMBER"]
-CC_EXP_MONTH = os.environ["CC_EXP_MONTH"]
-CC_EXP_YEAR = os.environ["CC_EXP_YEAR"]
-CC_CVV = os.environ["CC_CVV"]
+CREDIT_CARD_NUMBER = os.getenv("CREDIT_CARD_NUMBER", "4111111111111111")
+CC_EXP_MONTH = os.getenv("CC_EXP_MONTH", "12")
+CC_EXP_YEAR = os.getenv("CC_EXP_YEAR", "2030")
+CC_CVV = os.getenv("CC_CVV", "123")
